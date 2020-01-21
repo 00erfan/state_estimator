d14893024f1c6945f063d40aee5efb32e43108e6
Erfan Shojaei Barjuei <erfan.shojaei@iit.it>
2020-01-21 16:20:12 +0100

    observabilty finished !

--- plot_state.m
+++ plot_state.m
@@ -1,9 +1,11 @@
-
 %%
 REF_DATA = ref_data.signals.values;
 EST_DATA = est_trq.signals.values;
 ACT_DATA = act_trq.signals.values;
+MOT_TRQ = motor_trq.signals.values;
 TEMPO = ref_data.time;
+
+
 
 %%
 figure
@@ -13,7 +15,7 @@ plot( TEMPO , REF_DATA  , '--r ' ,'LineWidth' , 5);grid;shg
 plot( TEMPO , EST_DATA  , ':b' , 'LineWidth' , 5);grid;shg
 plot( TEMPO , ACT_DATA  , 'k','LineWidth' , 5);grid;shg
 
-xlabel('Time [Sec]')
+xlabel('Time [s]')
 ylabel('Torque [Nm]')
 legend('Reference Torque' , 'Estimated Torque' , 'Actual Torque ' )
 grid on
@@ -22,6 +24,14 @@ figure
 hold on
 set(gca,'FontSize',25)
 plot( TEMPO , ACT_DATA - EST_DATA   , 'k','LineWidth' , 5);grid;shg
-xlabel('Time [Sec]')
+xlabel('Time [s]')
 ylabel('Error on \Delta \tau_{h} [Nm]')
+grid on
+%%
+figure
+hold on
+set(gca,'FontSize',25)
+plot( TEMPO , MOT_TRQ   , 'k','LineWidth' , 5);grid;shg
+xlabel('Time [s]')
+ylabel('Torque [Nm]')
 grid on
