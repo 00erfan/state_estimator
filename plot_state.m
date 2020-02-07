b8798a5803eaea5bd58ec8e4d9397ce9634ab8a6
Erfan Shojaei Barjuei <erfan.shojaei@iit.it>
2020-02-07 18:03:51 +0100

    send 2 Gustavo 2nd

--- plot_state.m
+++ plot_state.m
@@ -5,15 +5,17 @@ ACT_DATA = act_trq.signals.values;
 MOT_TRQ = motor_trq.signals.values;
 TEMPO = ref_data.time;
 
+INT_DATA = int_trq.signals.values;
+INT_EST_DATA = int_est_trq.signals.values;
 
 
 %%
 figure
 hold on
 set(gca,'FontSize',25)
-plot( TEMPO , REF_DATA  , '--r ' ,'LineWidth' , 5);grid;shg
-plot( TEMPO , EST_DATA  , ':b' , 'LineWidth' , 5);grid;shg
-plot( TEMPO , ACT_DATA  , 'k','LineWidth' , 5);grid;shg
+plot( TEMPO , REF_DATA  , '--k ' ,'LineWidth' , 5);grid;shg
+plot( TEMPO , EST_DATA  , '-.b' , 'LineWidth' , 5);grid;shg
+plot( TEMPO , ACT_DATA  , ':r','LineWidth' , 5);grid;shg
 
 xlabel('Time [s]')
 ylabel('Torque [Nm]')
@@ -23,9 +25,12 @@ grid on
 figure
 hold on
 set(gca,'FontSize',25)
-plot( TEMPO , ACT_DATA - EST_DATA   , 'k','LineWidth' , 5);grid;shg
+%plot( TEMPO , ((ACT_DATA - EST_DATA)./ACT_DATA)*100  , 'k','LineWidth' , 5);grid;shg
+plot( TEMPO , (ACT_DATA - EST_DATA)  , 'k','LineWidth' , 5);grid;shg
+
 xlabel('Time [s]')
-ylabel('Error on \Delta \tau_{h} [Nm]')
+%ylabel('Error on \Delta \tau_{h} [%]')
+ylabel('Error on \Delta y_{b}')
 grid on
 %%
 figure
@@ -34,4 +39,27 @@ set(gca,'FontSize',25)
 plot( TEMPO , MOT_TRQ   , 'k','LineWidth' , 5);grid;shg
 xlabel('Time [s]')
 ylabel('Torque [Nm]')
+grid on
+
+%%
+figure
+hold on
+set(gca,'FontSize',25)
+plot( TEMPO , INT_EST_DATA  , '-.b' , 'LineWidth' , 5);grid;shg
+plot( TEMPO , INT_DATA  , ':r','LineWidth' , 5);grid;shg
+
+xlabel('Time [s]')
+ylabel('Velocity [rad/s]')
+legend('Estimated Velocity' , 'Actual Velocity' )
+grid on
+
+figure
+hold on
+set(gca,'FontSize',25)
+%plot( TEMPO , ((ACT_DATA - EST_DATA)./ACT_DATA)*100  , 'k','LineWidth' , 5);grid;shg
+plot( TEMPO , (INT_DATA -INT_EST_DATA)  , 'k','LineWidth' , 5);grid;shg
+
+xlabel('Time [s]')
+%ylabel('Error on \Delta \tau_{h} [%]')
+ylabel('Error on \Delta \theta_{s int}')
 grid on
